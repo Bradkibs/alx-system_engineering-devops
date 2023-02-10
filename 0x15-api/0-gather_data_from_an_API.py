8c3667820239f7685eb0a9664e3ee3fa2305865d
Bradleykibs <Bradleykibs@gmail.com>
2023-02-10 19:42:57 +0300

    pep8 compliancy

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,27 +8,28 @@ import requests
 import sys
 
 
-url_1 = 'https://jsonplaceholder.typicode.com/todos'
-url_2 = 'https://jsonplaceholder.typicode.com/users'
-payload = {'userId': str(sys.argv[1])}
-try:
-    req_1 = requests.get(url_2)
-    json_1 = req_1.json()
-    idx = int(sys.argv[1])
-    count = 0
-    completed = 0
-    titles = []
-    for i in json_1:
-        if i.get("id") == idx:
-            name = i.get("name")
-    req_2 = requests.get(url_1, params=payload)
-    json_2 = req_2.json()
-    for j in json_2:
-        count += 1
-        if j.get("completed"):
-            completed += 1
-            titles.append(j.get("title"))
-    print(f"Employee {name} is done with tasks({completed}/{count}):")
-    [print("\t" + " " + t) for t in titles]
-except Exception as e:
-    print("Error! " + str(e))
+if __name__ == '__main__':
+    url_1 = 'https://jsonplaceholder.typicode.com/todos'
+    url_2 = 'https://jsonplaceholder.typicode.com/users'
+    payload = {'userId': str(sys.argv[1])}
+    try:
+        req_1 = requests.get(url_2)
+        json_1 = req_1.json()
+        idx = int(sys.argv[1])
+        count = 0
+        completed = 0
+        titles = []
+        for i in json_1:
+            if i.get("id") == idx:
+                name = i.get("name")
+        req_2 = requests.get(url_1, params=payload)
+        json_2 = req_2.json()
+        for j in json_2:
+            count += 1
+            if j.get("completed"):
+                completed += 1
+                titles.append(j.get("title"))
+        print(f"Employee {name} is done with tasks({completed}/{count}):")
+        [print("\t" + " " + t) for t in titles]
+    except Exception as e:
+        print("Error! " + str(e))
