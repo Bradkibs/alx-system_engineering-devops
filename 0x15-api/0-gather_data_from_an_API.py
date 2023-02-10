9a60dd5ba308f9ff4767600475483d08dc799fdd
Bradleykibs <Bradleykibs@gmail.com>
2023-02-10 19:10:53 +0300

    gathering data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+A script to return information about TODO list progress of employees given
+their ID's
+"""
+
+import requests
+import sys
+
+
+url_1 = 'https://jsonplaceholder.typicode.com/todos'
+url_2 = 'https://jsonplaceholder.typicode.com/users'
+payload = {'userId': str(sys.argv[1])}
+try:
+    req_1 = requests.get(url_2)
+    json_1 = req_1.json()
+    id = int(sys.argv[1])
+    count = 0
+    completed = 0
+    titles = []
+    for i in json_1:
+        if (i.get("id") == id):
+            name = i.get("name")
+    req_2 = requests.get(url_1, params=payload)
+    json_2 = req_2.json()
+    for j in json_2:
+        count += 1
+        if (j.get("completed")):
+            completed += 1
+            titles.append(j.get("title"))
+    print(f"Employee {name} is done with tasks({completed}/{count}):")
+    [print("\t" + " " + t) for t in titles]
+except Exception as e:
+    print("Error! " + str(e))
