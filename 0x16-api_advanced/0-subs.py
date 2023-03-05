63d84c8860818d63aa8dda3495f236eb138a7188
Bradleykibs <Bradleykibs@gmail.com>
2023-03-05 08:33:14 +0300

    pycodestyle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
+Queries the Reddit API and returns the
+number of subscribers for a given subreddit.
 """
 
 
@@ -9,8 +10,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers on a subreddit """
-        
-        
+
     header = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=header, allow_redirects=False)
