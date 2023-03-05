1e7b21a3d33c6ed59d735a918f3b423b997926cf
Bradleykibs <Bradleykibs@gmail.com>
2023-03-05 13:57:40 +0300

    10 hot subreddits

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
+Queries the Reddit API and returns the
+number of subscribers for a given subreddit.
 """
 
 
@@ -10,6 +11,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
+
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API to print the
+title of the first 10 hot posts listed for a given
+subreddit
+"""
+
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """
+    returns a valid list of 10 hot titles of the subreddit
+    passed as argument. If not a valid subreddit will print
+    None
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    header = {"User-Agent": "Mozilla/5.0"}
+    resp = requests.get(url, headers=header, allow_redirects=False)
+    titles = ["nothing here"]
+
+    if resp.status_code == 200:
+        children = resp.json().get("data").get("children")
+        titles = [child.get("data").get("title") for child in children]
+        output = "\n".join(titles)
+        print(output)
+        return output
+    else:
+        return None
